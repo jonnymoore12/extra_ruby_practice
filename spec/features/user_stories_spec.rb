5bb42115b70e4681846aca8ab2c64bed2e4e1c8f
Jonny Moore <jonnymoore121212@gmail.com>
2016-09-28 12:49:49 +0100

    Completes User Story 2

--- spec/features/user_stories_spec.rb
+++ spec/features/user_stories_spec.rb
@@ -1,10 +1,19 @@
 describe 'User Stories' do
   # As an air traffic controller
   # So I can get passengers to a destination
-  # I want to instruct a plane to land at an airport and confirm that it has landed
+  # I want to instruct a plane to land at an airport
   it 'so planes land at airports, instruct a plane to land' do
     airport = Airport.new
     plane = Plane.new
     expect { airport.land(plane) }.not_to raise_error
+  end
+
+  # As an air traffic controller
+  # So I can get passengers on the way to their destination
+  # I want to instruct a plane to take off from an airport
+  it 'so planes take off from an airport, instruct a plane to take off' do
+    airport = Airport.new
+    plane = Plane.new
+    expect { airport.take_off(plane) }.not_to raise_error
   end
 end
